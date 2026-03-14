15909324876639d28173212df9ca3abd49c2b8ff
ĐĂNG KHOA <NG KHOA@LAPTOP-O0VF9BT9>
2026-03-14 09:18:37 +0700

    Add project files.

---
+++ WebQLPT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebQLPT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
